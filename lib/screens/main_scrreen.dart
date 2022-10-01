ec0f6e026ae69bef4b507389e638c4690b679604
Granitin <granitin@inbox.ru>
2022-10-01 20:19:57 +0300

    com3

--- lib/screens/main_scrreen.dart
+++ lib/screens/main_scrreen.dart
@@ -34,7 +34,7 @@ class TopMenu extends StatelessWidget {
         LayoutId(
           id: 1,
           child: Text(
-            'Pacifico font example',
+            'Pacifico font for example',
             style: GoogleFonts.pacifico(fontSize: 20, color: Colors.brown),
           ),
         ),
